79c49f44c13cdae71937d11bc2a2d90bf6fc617d
jake-pauls <jakepauls99@gmail.com>
2023-03-04 19:29:57 -0800

    Gave up on native interop between api/wave, trying something new

---
+++ api/src/Main.c
@@ -0,0 +1,8 @@
+#include "surf/Bridge.h"
+
+int main(int argc, char** argv)
+{
+    surf_Init();
+
+    return 0;
+}
--- c-interop-example/base.ml
+++
@@ -1,7 +0,0 @@
-(** Code taken from sample on https://v2.ocaml.org/manual/intfc.html *)
-
-let rec fib n = if n < 2 then 1 else fib (n - 1) + fib (n - 1)
-let fmt_res n = Printf.sprintf "Result is: %d\n" n
-
-let _ = Callback.register "fib" fib
-let _ = Callback.register "fmt_res" fmt_res
